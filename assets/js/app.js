67757e46170f01b65f02435087577282093c0402
Hajime <millarayzunigag@gmail.com>
2024-03-05 17:31:04 -0300

    primer commit index

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola a mis panas de JS</h1>
+
+    <a href="https://app.diagrams.net/#G1UVqwOKVDQvNFYMC9zRbA_e0yHbo9sCxA#%7B%22pageId%22%3A%22LkbXasa-NwSJNSEpSG1r%22%7D"></a>
+
+    <script src="./assets/js/app.js"></script>
+</body>
+</html>
