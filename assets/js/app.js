54c29496a441b05d079306b3a70c6abab922164f
Hajime <millarayzunigag@gmail.com>
2024-03-05 17:32:08 -0300

    segundo commit js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola a mis panas de JS</h1>
+console.log("activacion")
 
-    <a href="https://app.diagrams.net/#G1UVqwOKVDQvNFYMC9zRbA_e0yHbo9sCxA#%7B%22pageId%22%3A%22LkbXasa-NwSJNSEpSG1r%22%7D"></a>
+const numeroUno = +prompt("numero 1")
+const numeroDos = +prompt("numero 2")
 
-    <script src="./assets/js/app.js"></script>
-</body>
-</html>
+const resultadoSuma = numeroUno + numeroDos
+const resultadoResta = numeroUno - numeroDos
+const resultadoMultiplicacion = numeroUno * numeroDos
+const resultadoDivision = numeroUno / numeroDos 
+const resultadoModulo = numeroUno % numeroDos
+
+alert(resultadoSuma)
+alert(resultadoResta)
+alert(resultadoMultiplicacion)
+alert(resultadoDivision)
+alert(resultadoModulo)
+
+
+// Programa grados celsius 
+const temperaturaC = +prompt("Ingrese temperatura en grados celsius")
+
+const temperaturaF = ((temperaturaC+273.15)/273.15)
+const temperaturaK = ((temperaturaC*9/5+32)/32)
+
+alert(temperaturaF)
+alert(temperaturaK)
+
+//Usuario cantidad de días, años, semanas, días
+
+const diasUsuario = +prompt("Ingrese una cantidad de días")
+
+const años = Math.floor(diasUsuario/365)
+
+const semanas = Math.floor((diasUsuario-365*años)/7)
+
+const dias = diasUsuario-años*365-semanas*7
+
+alert(años)
+alert(semanas)
+alert(dias)
+
+//sumatoria 5 números
+
+const primerNumero = +prompt("n° 1")
+const segundoNumero = +prompt("n° 2")
+const tercerNumero = +prompt("n° 3")
+const cuartoNumero = +prompt ("n° 4")
+const quintoNumero = +prompt ("n°5")
+
+const sumaTotal = primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero
+
+const promedio = sumaTotal / 5
+
+alert(sumaTotal)
+alert(promedio)
